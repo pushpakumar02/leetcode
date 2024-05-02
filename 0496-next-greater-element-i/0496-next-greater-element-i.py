893e33ae5738e302dac6c604537a4f90d5532928
Pushpa Kumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-05-02 13:49:52 +0530

    Update 0496-next-greater-element-i.py

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,8 +1,23 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1Idx = {n : i for i,n in enumerate(nums1)}
+        # optimal: O(m + n)
+        nums1Idx = {n : i for i,n in enumerate(nums1)}  # [(0, 'apple'), (1, 'banana'), (2, 'cherry')]
         res = [-1] * len(nums1)
+        stack  = [] 
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = cur
+            if nums2[i] in nums1Idx:
+                    stack.append(cur)
+        return res                         # Time complexity: O(m + n) | space complexity: O(m)
 
+
+        # Brute_force: O(m * n)
+        nums1Idx = {n : i for i,n in enumerate(nums1)} 
+        res = [-1] * len(nums1)
         for i in range(len(nums2)):
             if nums2[i] not in nums1Idx:
                 continue
@@ -11,5 +26,5 @@ class Solution:
                     idx = nums1Idx[nums2[i]]
                     res[idx] = nums2[j]
                     break
-        return res
+        return res                         # Time complexity: O(m * n) | space complexity: O(m)
                     
