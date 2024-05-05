5b8dc30e5e9071078a0a79c1820d9257697bdc4c
Pushpa Kumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-05-05 16:59:18 +0530

    🔼Update Intuition and Approach - Array

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -6,3 +6,19 @@ class Solution:
             arr[i] = rightMax 
             rightMax = newMax
         return arr
+
+
+# Intuition and Approach:
+
+# - We're given an array `arr`.
+# - For each element in `arr`, we want to replace it with the maximum element to its right, and for the last element, we replace it with -1.
+# - We initialize `rightMax` to -1 to keep track of the maximum element seen so far.
+# - We iterate through `arr` in reverse order, starting from the second-to-last element.
+# - For each element at index `i`, we find the maximum between `rightMax` and `arr[i]`. This represents the maximum element to the right of `arr[i]`.
+# - We update `arr[i]` with the current value of `rightMax`, as it represents the maximum element to the right of `arr[i]`.
+# - We update `rightMax` with the new maximum value (`newMax`) found in the current iteration.
+# - Finally, we return `arr` with the elements replaced.
+
+# Time Complexity: O(n) - where n is the length of `arr`. We iterate through `arr` once.
+
+# Space Complexity: O(1) - We use only a constant amount of extra space for variables (`rightMax` and `newMax`) regardless of the size of `arr`.
