37f57aba5e9a98664c2ddd2455c2788922173b85
Pushpa Kumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-05-05 14:02:58 +0530

    🔼Update Intuition and Approach

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,4 +8,19 @@ class Solution:
                 return[prevMap[diff],i]
             prevMap[n]=i
         return
+
+
+# Intuition and Approach:
         
+# - We're given an array of integers `nums` and a target integer.
+# - The task is to find two numbers in `nums` that add up to the target.
+# - We use a hashmap (dictionary in Python) `prevMap` to store the indices of previously encountered numbers.
+# - We iterate through each number in `nums` along with its index.
+# - For each number `n`, we calculate the difference `diff` between the target and `n`.
+# - If `diff` exists in `prevMap`, it means we've found the complement for `n`, and we return the indices of `diff` and `n`.
+# - Otherwise, we store `n` in `prevMap` along with its index.
+# - If no solution is found after iterating through `nums`, we return an empty list.
+
+# Time Complexity: O(n) - where n is the length of `nums`. We iterate through `nums` once.
+
+# Space Complexity: O(n) - where n is the length of `nums`. In the worst case, `prevMap` contains all elements of `nums`.
