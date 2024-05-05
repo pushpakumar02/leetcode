e128bdfd6614d4abfb212710d768951ef92b389d
Pushpa Kumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-05-05 17:32:51 +0530

    🔼Update Intuition and Approach - Array

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -7,3 +7,18 @@ class Solution:
             hashset.add(i)
         return False
                 
+
+# Intuition and Approach:
+
+# - We're given a list of integers `nums`.
+# - The task is to determine whether there are any duplicate elements in `nums`.
+# - We use a set `hashset` to keep track of unique elements encountered so far.
+# - We iterate through each element `i` in `nums`.
+# - For each element `i`, we check if it exists in `hashset`.
+#   - If it does, it means `i` is a duplicate, so we return `True`.
+#   - If not, we add `i` to `hashset`.
+# - If we complete the loop without finding any duplicates, we return `False`.
+
+# Time Complexity: O(n) - where n is the length of `nums`. We iterate through `nums` once.
+
+# Space Complexity: O(n) - where n is the length of `nums`. In the worst case, all elements of `nums` are unique, so the space used by `hashset` will be equal to the size of `nums`.
