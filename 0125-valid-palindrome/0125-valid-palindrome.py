3e621f9c96d7ddbd743f2e3f2d654a00d304013d
Pushpa Kumar <112679167+Pushpakumar02@users.noreply.github.com>
2024-05-06 11:03:42 +0530

    🔼Update Intuition and Approach - Two Pointers

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -15,5 +15,21 @@ class Solution:
     def alphaNum(self, n):
         return (ord('A') <= ord(n) <= ord('Z') or 
                 ord('a') <= ord(n) <= ord('z') or 
-                ord('0') <= ord(n) <= ord('9'))       #Space Complexity: O(n) | Time Complexity: O(1)
+                ord('0') <= ord(n) <= ord('9'))      
 
+# Intuition and Approach:
+
+# - The task is to determine whether a given string `s` is a palindrome, considering only alphanumeric characters and ignoring cases.
+# - We use a two-pointer approach, `l` starting from the beginning of the string and `r` starting from the end.
+# - While `l` is less than `r`, we move both pointers towards each other.
+# - At each step:
+#   - We move `l` to the right until it points to an alphanumeric character, using the `alphaNum` helper function.
+#   - We move `r` to the left until it points to an alphanumeric character, using the `alphaNum` helper function.
+#   - We check if the characters at `l` and `r` (converted to lowercase) are equal.
+#     - If they are not equal, `s` is not a palindrome, and we return `False`.
+#   - We move `l` to the right and `r` to the left to continue the comparison.
+# - If we complete the loop without finding any mismatch, `s` is a palindrome, and we return `True`.
+
+# Time Complexity: O(n) - where n is the length of the input string `s`. We iterate through `s` once with the two-pointer approach.
+
+# Space Complexity: O(1) - We use only a constant amount of extra space regardless of the size of `s`.
